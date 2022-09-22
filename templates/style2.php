b91029ecf4e319330228f001a8f900d62c3e7a51
Alan <a49.piron@gmail.com>
2022-09-22 11:32:59 +0200

    first

---
+++ templates/style2.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Template Name: Style 2
+ * Template Post Type: post, page
+ *
+ */
+
+get_header();
+?>
+
+<main class="main-content">
+
+	<?php the_content() ?>
+
+</main>
+
+<?php get_footer(); ?>
